6b2d19061e3c21620bc517ddebdee6f44df55452
Ashwini Surve <ashwinisurve@Ashwinis-MacBook-Air.local>
2024-08-06 16:00:34 -0400

    nextjs initial commit

---
+++ src/app/tickets/Ticket.model.ts
@@ -0,0 +1,7 @@
+export interface Ticket {
+    id: string;
+    title: string;
+    body: string;
+    priority: string;
+    user_email: string;
+};
---
+++ src/app/tickets/create/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import CreateForm from "./CreateForm"
+
+export default function CreateTicket() {
+    return (
+        <main>
+            <h2 className="text-primary text-center">Add a new ticket</h2>
+            <CreateForm />
+        </main>
+    )
+}
